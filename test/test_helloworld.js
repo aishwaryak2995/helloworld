c402b365b7c06e71e0e544c90127f08f51a62088
aishwaryak2995 <aishwaryak2995@gmail.com>
2023-10-15 21:13:25 +0200

    update unit test execution

---
+++ test/test_helloworld.js
@@ -0,0 +1,16 @@
+const helloworldFunction = require("../helloworld");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("helloworldFunction", () => {
+
+  it("should return 'helloworld'", () => {
+    const instance = new helloworldFunction();
+    const result = instance.helloworldFunction();
+    expect(result).to.equal("helloworld");
+
+  })
+})
+
+
+
